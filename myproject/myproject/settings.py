546d9bbe9dbfc79bc486200f99b80b7db781cbba
devarsh.s <devarsh.s@latitudetechnolabs.com>
2023-03-13 19:10:46 +0530

    signup and logout is added

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'widget_tweaks',
-    
+    'accounts',
     'boards',
 ]
 
@@ -131,3 +131,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = 'home'
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -17,12 +17,16 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import re_path
 # from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
+from accounts import views as accounts_views
 from boards import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
+    path('signup/',accounts_views.signup, name='signup'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
 ]
