c673a495b07daf41de4e49a9d0842c0ab5669e28
devarsh.s <devarsh.s@latitudetechnolabs.com>
2023-03-13 14:25:58 +0530

    made form for inserting posts

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -23,6 +23,6 @@ from boards import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
-    # path('boards/<int:pk>/',views.boards_topics)
-    re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics')
+    re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
 ]
